6cd39cf6e6adacf0fc7cb0543f38e5dc66578f2e
md2eoseo <md2eoseo@gmail.com>
2020-03-17 20:18:15 +0100

    exercise 3

--- beachball.js
+++ beachball.js
@@ -3,6 +3,7 @@
 import { gsap } from "gsap";
 
 const ball = document.querySelector(".ball");
-gsap.to(ball, { translateX: 400, duration: 2 });
-gsap.to(ball, { rotate: 200, duration: 2 });
-gsap.to(ball, { delay: 2, scale: 0, translateY: "5vw", duration: 1 });
+const timeline = gsap.timeline({ repeat: 2, repeatDelay: 1 });
+
+timeline.to(ball, { translateX: 400, rotate: 200, duration: 2 });
+timeline.to(ball, { scale: 0, translateY: "5vw", duration: 1 });
