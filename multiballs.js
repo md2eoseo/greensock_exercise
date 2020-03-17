9bda75ecc28b410468cbf59078bf701aae274990
md2eoseo <md2eoseo@gmail.com>
2020-03-17 19:27:53 +0100

    exercise 2 just for one bounce

--- multiballs.js
+++ multiballs.js
@@ -1,1 +1,27 @@
 "use strict";
+
+import { gsap } from "gsap";
+
+const ball = document.querySelectorAll(".ball");
+let height = 80;
+let bounce_duration = 2;
+let scale_duration = 0.1;
+
+gsap.from(ball, { y: `-${height}vh`, duration: bounce_duration });
+gsap.to(ball, {
+  delay: bounce_duration,
+  scaleX: 1.5,
+  scaleY: 0.5,
+  duration: scale_duration
+});
+gsap.to(ball, {
+  delay: bounce_duration + scale_duration,
+  scaleX: 1,
+  scaleY: 1,
+  duration: scale_duration
+});
+gsap.to(ball, {
+  delay: bounce_duration + scale_duration + scale_duration,
+  translateY: `-${height}vh`,
+  duration: bounce_duration
+});
