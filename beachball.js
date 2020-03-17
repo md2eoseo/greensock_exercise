125ce658712781302ae818c607ea43d0142e3523
md2eoseo <md2eoseo@gmail.com>
2020-03-17 18:55:07 +0100

    exercise 1

--- beachball.js
+++ beachball.js
@@ -1,1 +1,8 @@
 "use strict";
+
+import { gsap } from "gsap";
+
+const ball = document.querySelector(".ball");
+gsap.to(ball, { translateX: 400, duration: 2 });
+gsap.to(ball, { rotate: 200, duration: 2 });
+gsap.to(ball, { delay: 2, scale: 0, translateY: "5vw", duration: 1 });
